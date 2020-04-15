cb76359b9c069179f5a466007f3d04cebe466670
Bryan Correa <bralco03@hotmail.com>
2020-04-15 15:18:05 -0600

    Added new component called welcome, after that component there will be a section dedicated to videos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,6 @@ const Navbar = () => {
                 <div className="container">
                     <a href="#" className="brand-logo">PLACEHOLDER</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a>Nuevo</a></li>
                         <li><a>Galeria</a></li>
                         <li><a>Participa</a></li>
                         <li><a>Sobre Nosotros</a></li>
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import './welcome.css';
 
 const Welcome = () => {
-   
+   return(
+       <div className="welcome valign-wrapper">
+           <div className="welcome-text">
+           <h4>Bienvenido</h4>
+           <p className="flow-text">
+            "La música es la mediadora entre el mundo 
+           espiritual y el de los sentidos"</p>
+           </div>
+       </div>
+   )
 }
 
 

93cf5c5148aab09ef4574f4d0d4cc907823ed123
Bryan Correa <bralco03@hotmail.com>
2020-05-12 18:57:55 -0600

    Created the musicians tab, now fully styled. still need to develop the final tab

--- src/components/SendVideo.jsx
+++ src/components/SendVideo.jsx
@@ -7,13 +7,30 @@ const SendVideo  = () => {
         <div className="bg-div">
             
             <div className="  div-form">
+                <div>
+                    <h1 className="center">Musico</h1>
+                    <p className="center">No dudes en mandarnos tus videos. Seran revisados y seleccionados
+                        para nuestra pagina. Se te enviara un correo de confirmacion.
+                        Asi sabras cuando se suba.
+                    </p>
+                </div>
+                
+                <div>
                 <form>
                     <input type="text" placeholder="Name"/>
                     <input type="email" placeholder="E-Mail"/>
                     <input type="text" placeholder="Video title"/>
-                    <input type="file" placeholder="Drag video"/>
-                    <button>Submit</button>
+                    <div className="center">
+                      <label className="archivoCSS">
+                          Subir Video
+                        <input type="file" placeholder="Drag video" className="archivo"/>
+                      </label>
+                    </div>
+                    <div className="center">
+                      <button>Submit</button>
+                    </div>
                 </form>
+                </div>
             </div>
             <img src={musicians} alt="musicians"/>
         </div>
